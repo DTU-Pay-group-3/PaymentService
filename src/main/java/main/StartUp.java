1b5e4b5b2a14cacb1f59e6829f01dfe90266ba6b
MarianCCulea <marianclaudiu.culea@gmail.com>
2024-01-18 11:14:18 +0100

    :)

--- src/main/java/main/StartUp.java
+++ src/main/java/main/StartUp.java
@@ -1,28 +1,14 @@
 package main;
 
-import dtu.ws.fastmoney.AccountInfo;
-import dtu.ws.fastmoney.BankService;
-import dtu.ws.fastmoney.BankServiceService;
-import jakarta.json.Json;
 import messaging.implementations.RabbitMqQueue;
-
-import javax.swing.text.html.parser.Entity;
-import java.util.ArrayList;
 
 public class StartUp {
 	public static void main(String[] args) throws Exception {
-
-		 BankService bank = new BankServiceService().getBankServicePort();
-	ArrayList<AccountInfo> acc=new ArrayList<>(bank.getAccounts());
-		System.out.println(acc.size());
-		//new StartUp().startUp();
+		new StartUp().startUp();
 	}
 
 	private void startUp() throws Exception {
-
 		var mq = new RabbitMqQueue("rabbitMq");
-		System.out.println("connected to Mq");
-		System.out.println(mq.toString());
 		new PaymentService(mq);
 	}
 }
